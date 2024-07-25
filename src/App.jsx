d7f9a3c110bb9ef39415594b63b23bea6c36d4e8
Gabriel Rodrigo <gabrielrodrigo252004@gmail.com>
2024-07-25 14:43:37 -0300

    enviando repositorio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+import Card from './assets/Card'
+
+function App() {
+
+  return (
+    
+    <div>
+      <Card titulo='Ola Mundo!'>
+        <p>Criando cards para facilitar a organização de nossos exercícios</p>
+      </Card>
+    </div>
+  )
+}
+
+export default App
